8e72462ed673dc033a501fbfa66a964f916ef925
vladimircuriel <vladimircuriel@outlook.com>
2025-09-29 22:23:52 -0400

    feat(data): add all page information constants

---
+++ src/lib/data.ts
@@ -0,0 +1,68 @@
+const NUMBERS = [
+  {
+    value: '260+',
+    description: 'Personas por Evento',
+  },
+  {
+    value: '12',
+    description: 'Ediciones',
+  },
+  {
+    value: '25+',
+    description: 'Desconferencias Simultáneas',
+  },
+  {
+    value: '10+',
+    description: 'Años',
+  },
+]
+
+const ABOUT = [
+  {
+    title: '¿Qué es BarCamp?',
+    description:
+      'Es una red internacional de "desconferencias" tecnológicas, cuyo contenido es auto-generado por los participantes y su orientación es hacia el desarrollo de Software y tecnología en general.',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: '¿Quién puede participar?',
+    description:
+      'Cualquier desarrollador de software, profesores y estudiantes de distintas universidades pueden participar. ¡El único requerimiento es tener ganas de aprender más!',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+]
+
+const NAV_ITEMS = [
+  {
+    href: '#inicio',
+    text: 'Inicio',
+  },
+  {
+    href: '#acerca',
+    text: 'Acerca de',
+  },
+  // {
+  //   href: "#invitado",
+  //   text: "Invitado",
+  // },
+  // {
+  //   href: "#desconferencias",
+  //   text: "Desconferencias",
+  // },
+  // {
+  //   href: "#charlistas",
+  //   text: "Charlistas",
+  // },
+  // {
+  //   href: "#patrocinadores",
+  //   text: "Patrocinadores",
+  // },
+  {
+    href: '#código-de-conducta',
+    text: 'Código de Conducta',
+  },
+]
+
+export { NUMBERS, ABOUT, NAV_ITEMS }
